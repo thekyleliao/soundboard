1be212771c2acf1d6c590e21bfd2eb93caae8855
Kyle Liao <thekyleliao@gmail.com>
2023-08-25 18:00:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const card = document.querySelector(".card");
+card.addEventListener("click", function () {
+  // Perform desired action when the card is clicked
+  console.log("Button clicked!");
+});
+
+var horn = new Audio("/sounds/horn.mp3");
+
+var welcome = new Audio("/sounds/welcome.mp3");
+
+var ok = new Audio("/sounds/ok.mp3");
+
+var questions = new Audio("/sounds/questions.mp3");
+
+var nextweek = new Audio("/sounds/nextweek.mp3");
+
+var first = new Audio("/sounds/first.mp3");
+
+var goodtime = new Audio("/sounds/goodtime.mp3");
