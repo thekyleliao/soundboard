543aca755afd9ae0fdcef744e053b68aade4a427
Kyle Liao <thekyleliao@gmail.com>
2023-08-31 15:17:38 -0400

    Finished

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ var nextweek = new Audio("/sounds/nextweek.mp3");
 var first = new Audio("/sounds/first.mp3");
 
 var goodtime = new Audio("/sounds/goodtime.mp3");
+
+var greatidea = new Audio("/sounds/greatidea.mp3");
+
+var yeah = new Audio("/sounds/yeah.mp3");
+
+var lightwork = new Audio("/sounds/lightwork.mp3");
+
+var what = new Audio("/sounds/what.mp3");
+
+var mychoice = new Audio("/sounds/mychoice.mp3");
+
+var anger = new Audio("/sounds/anger.mp3");
